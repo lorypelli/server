d9995b1672c160d338b98475010a0057c339f3b8
lorypelli <lorypelli@proton.me>
2024-10-14 20:18:37 +0200

    fix when html extension is disabled

--- start.go
+++ start.go
@@ -19,18 +19,22 @@ func Start(dir, name string, extension bool, port uint16) {
 		path := ctx.Path()
 		t := ctx.Query("t")
 		time := time.Now().Unix()
-		if !extension {
-			if !strings.Contains(path, ".") {
-				if err := ctx.SendFile(dir + path + ".html"); err == nil {
-					return nil
-				}
-			}
-		}
 		if strings.TrimSpace(t) == "" || t < pterm.Sprint(time) {
 			return ctx.Redirect(pterm.Sprintf("%s?t=%d", path, time))
 		}
 		return ctx.Next()
 	})
+	if !extension {
+		app.Use(func(ctx *fiber.Ctx) error {
+			path := ctx.Path()
+			if !strings.Contains(path, ".") {
+				if err := ctx.SendFile(dir + path + ".html"); err == nil {
+					return nil
+				}
+			}
+			return ctx.Next()
+		})
+	}
 	app.Static("/", dir)
 	if err := app.Listen(pterm.Sprintf("127.0.0.1:%d", port)); err != nil {
 		pterm.Error.Println(err)
