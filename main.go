274c58f562ff7eafa731504e86ac9d2dcca1fabc
lorypelli <lorypelli@proton.me>
2024-10-20 10:00:56 +0200

    use different url for realtime and enable it only on a certain condition

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 		os.Exit(1)
 	}
 	extension, _ := pterm.DefaultInteractiveConfirm.WithDefaultText("Do you want to use the .html extension?").WithDefaultValue(true).Show()
+	realtime, _ := pterm.DefaultInteractiveConfirm.WithDefaultText("Do you want to have realtime loading for .html files?").Show()
 	if strings.TrimSpace(*name) == "" {
 		*name, _ = pterm.DefaultInteractiveTextInput.WithDefaultText("Provide app name").Show()
 	}
@@ -36,5 +37,10 @@ func main() {
 		pterm.Error.Println(err)
 		os.Exit(1)
 	}
+	go func() {
+		if realtime {
+			StartWebsocket(*dir)
+		}
+	}()
 	Start(*dir, *name, extension, p)
 }
--- start.go
+++ start.go
@@ -16,14 +16,6 @@ func Start(dir, name string, extension bool, port int) {
 		AppName:      name,
 		ServerHeader: name,
 	})
-	app.Get("/ws", websocket.New(func(ctx *websocket.Conn) {
-		defer ctx.Close()
-		hasChanged := make(chan bool)
-		go Monitor(dir, hasChanged)
-		if <-hasChanged {
-			ctx.WriteMessage(websocket.TextMessage, []byte("reload"))
-		}
-	}))
 	if !extension {
 		app.Use(func(ctx *fiber.Ctx) error {
 			path := ctx.Path()
@@ -53,7 +45,7 @@ func Start(dir, name string, extension bool, port int) {
 			if err != nil {
 				return ctx.Next()
 			}
-			body = []byte(strings.ReplaceAll(string(body), "</body>", pterm.Sprintf("<script>new WebSocket('ws://127.0.0.1:%d/ws').onmessage=e=>{e.data=='reload'&&location.reload()}</script></body>", port)))
+			body = []byte(strings.ReplaceAll(string(body), "</body>", "<script>new WebSocket('ws://127.0.0.1:50643').onmessage=e=>{e.data=='reload'&&location.reload()}</script></body>"))
 			ctx.Set("Content-Type", "text/html")
 			return ctx.Send(body)
 		}
@@ -61,6 +53,24 @@ func Start(dir, name string, extension bool, port int) {
 	})
 	app.Static("/", dir)
 	if err := app.Listen(pterm.Sprintf("127.0.0.1:%d", port)); err != nil {
+		pterm.Error.Println(err)
+		os.Exit(1)
+	}
+}
+
+func StartWebsocket(dir string) {
+	app := fiber.New(fiber.Config{
+		DisableStartupMessage: true,
+	})
+	app.Get("/", websocket.New(func(ctx *websocket.Conn) {
+		defer ctx.Close()
+		hasChanged := make(chan bool)
+		go Monitor(dir, hasChanged)
+		if <-hasChanged {
+			ctx.WriteMessage(websocket.TextMessage, []byte("reload"))
+		}
+	}))
+	if err := app.Listen("127.0.0.1:50643"); err != nil {
 		pterm.Error.Println(err)
 		os.Exit(1)
 	}
