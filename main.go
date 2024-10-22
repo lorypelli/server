eab545aac897a5a3dcfb53f221ab6abbacd5655b
lorypelli <lorypelli@proton.me>
2024-10-22 18:06:57 +0200

    ability to choose html file extension

--- main.go
+++ main.go
@@ -13,30 +13,35 @@ const WS_PORT = 50643
 
 func main() {
 	dir := flag.String("d", "", "Directory to serve")
+	ext := flag.String("e", "", "Extension to use")
 	name := flag.String("n", "", "App name")
 	port := flag.String("p", "", "Port to use")
 	flag.Parse()
-	if strings.TrimSpace(*dir) == "" {
+	*dir = strings.TrimSpace(*dir)
+	*ext = strings.TrimSpace(*ext)
+	*name = strings.TrimSpace(*name)
+	*port = strings.TrimSpace(*port)
+	if *dir == "" {
 		*dir, _ = pterm.DefaultInteractiveTextInput.WithDefaultText("Provide directory to serve").WithDefaultValue(".").Show()
 	}
-	*dir = strings.TrimSpace(*dir)
 	if _, err := os.Stat(*dir); err != nil {
 		pterm.Error.Println(err)
 		os.Exit(1)
 	}
 	extension, _ := pterm.DefaultInteractiveConfirm.WithDefaultText("Do you want to use the HTML extension?").WithDefaultValue(true).Show()
 	realtime, _ := pterm.DefaultInteractiveConfirm.WithDefaultText("Do you want to have realtime loading for HTML files?").Show()
+	if *ext != ".html" && *ext != ".htm" {
+		*ext, _ = pterm.DefaultInteractiveSelect.WithDefaultText("Choose HTML extension").WithOptions([]string{".html", ".htm"}).Show()
+	}
 	if realtime {
 		pterm.Warning.Printfln("Port %d can't be used since it's in use by the realtime service!", WS_PORT)
 	}
-	if strings.TrimSpace(*name) == "" {
+	if *name == "" {
 		*name, _ = pterm.DefaultInteractiveTextInput.WithDefaultText("Provide app name").Show()
 	}
-	*name = strings.TrimSpace(*name)
-	if strings.TrimSpace(*port) == "" {
+	if *port == "" {
 		*port, _ = pterm.DefaultInteractiveTextInput.WithDefaultText("Provide port to use").WithDefaultValue("53273").Show()
 	}
-	*port = strings.TrimSpace(*port)
 	p, err := strconv.Atoi(*port)
 	if err != nil {
 		pterm.Error.Println(err)
@@ -47,5 +52,5 @@ func main() {
 			StartWebsocket(*dir, WS_PORT)
 		}
 	}()
-	Start(*dir, *name, extension, realtime, p, WS_PORT)
+	Start(*dir, *ext, *name, extension, realtime, p, WS_PORT)
 }
--- start.go
+++ start.go
@@ -11,7 +11,7 @@ import (
 	"github.com/pterm/pterm"
 )
 
-func Start(dir, name string, extension, realtime bool, port, ws_port int) {
+func Start(dir, ext, name string, extension, realtime bool, port, ws_port int) {
 	app := fiber.New(fiber.Config{
 		AppName:      name,
 		ServerHeader: name,
@@ -20,7 +20,7 @@ func Start(dir, name string, extension, realtime bool, port, ws_port int) {
 		app.Use(func(ctx *fiber.Ctx) error {
 			path := ctx.Path()
 			if !strings.Contains(path, ".") {
-				if err := ctx.SendFile(dir + path + ".html"); err == nil {
+				if err := ctx.SendFile(dir + path + ext); err == nil {
 					return nil
 				}
 			}
@@ -33,14 +33,14 @@ func Start(dir, name string, extension, realtime bool, port, ws_port int) {
 		path := ctx.Path()
 		file := strings.TrimPrefix(path, "/")
 		if file == "" {
-			file += "index.html"
+			file += "index" + ext
 		} else if !strings.Contains(file, ".") && !extension {
-			file += ".html"
+			file += ext
 		}
 		if t != time {
 			return ctx.Redirect(pterm.Sprintf("%s?t=%s", path, time))
 		}
-		if realtime && strings.HasSuffix(file, ".html") {
+		if realtime && strings.HasSuffix(file, ext) {
 			body, err := os.ReadFile(file)
 			if err != nil {
 				return ctx.Next()
@@ -51,7 +51,9 @@ func Start(dir, name string, extension, realtime bool, port, ws_port int) {
 		}
 		return ctx.Next()
 	})
-	app.Static("/", dir)
+	app.Static("/", dir, fiber.Static{
+		Index: "index" + ext,
+	})
 	if err := app.Listen(pterm.Sprintf("127.0.0.1:%d", port)); err != nil {
 		pterm.Error.Println(err)
 		os.Exit(1)
