4867475a6970df96ae83885f2245b4561ddd1a3d
lorypelli <lorypelli@proton.me>
2024-11-30 10:58:21 +0100

    use realtime loading by default

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,7 +15,7 @@ const (
 	WS_PORT                uint16 = 50643
 	DEFAULT_DIR                   = "."
 	DEFAULT_USE_EXT               = true
-	DEFAULT_USE_REALTIME          = false
+	DEFAULT_USE_REALTIME          = true
 	DEFAULT_EXPOSE_NETWORK        = true
 	DEFAULT_EXT                   = ".html"
 	DEFAULT_PORT                  = "53273"
