350e99bbe30af50bd4ef33b2d4840ba84f8f639d
lorypelli <lorypelli@proton.me>
2024-11-30 10:16:51 +0100

    normalize filepath

--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -1,7 +1,8 @@
 import { tmpdir } from 'node:os';
+import { normalize } from 'node:path';
 
 export const dir = tmpdir();
 
 export const extension = process.platform == 'win32' ? '.exe' : '';
 
-export const file = `${dir}/server${extension}`;
+export const file = normalize(`${dir}/server${extension}`);
