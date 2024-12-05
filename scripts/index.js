c39cf5724ee6135a222f23e431c788d5955a8ef1
lorypelli <lorypelli@proton.me>
2024-12-05 19:39:13 +0100

    use `execa` for better error handling

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-import { execFileSync } from 'node:child_process';
-import { error } from 'node:console';
+import { execa } from 'execa';
 import { existsSync } from 'node:fs';
 import { chmod } from 'node:fs/promises';
 import { argv, platform } from 'node:process';
@@ -9,6 +8,7 @@ import { dir, extension, file } from './utils/constants.js';
 import create from './utils/create.js';
 import del from './utils/delete.js';
 import download from './utils/download.js';
+import { error } from './utils/logs.js';
 import write from './utils/write.js';
 
 const url = `https://github.com/lorypelli/server/releases/latest/download/server_${platform}${extension}`;
@@ -17,4 +17,4 @@ if (!existsSync(dir)) await create(dir);
 if (existsSync(file)) await del(file);
 await write(file, buffer);
 chmod(file, 0o777).catch((err) => error(err));
-execFileSync(file, argv.slice(2), { stdio: 'inherit' });
+execa(file, argv.slice(2), { stdio: 'inherit' }).catch((err) => error(err));
