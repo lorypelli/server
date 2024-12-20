a6e99dc43b7fe94bff01d63808f6fce3119c98d4
lorypelli <lorypelli@proton.me>
2024-12-20 15:44:36 +0100

    finish custom logger

--- internal/logger.go
+++ internal/logger.go
@@ -1,6 +1,8 @@
 package internal
 
 import (
+	"errors"
+	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -12,10 +14,20 @@ func Logger() fiber.Handler {
 		err := ctx.Next()
 		now := time.Now().Format("15:04:05")
 		status := ctx.Response().StatusCode()
+		var e *fiber.Error
+		var str string
+		if err != nil && errors.As(err, &e) {
+			status = e.Code
+			str = e.Message
+		}
+		str = strings.TrimSpace(str)
 		method := ctx.Method()
 		path := ctx.Path()
 		ip := ctx.IP()
 		msg := pterm.Sprintf("%s (%d): %s - %s (IP: %s)", now, status, method, path, ip)
+		if str != "" {
+			msg += pterm.Sprintf(": %s", str)
+		}
 		if status >= 200 && status < 300 {
 			pterm.Success.Println(msg)
 		} else if status >= 300 && status < 400 {
