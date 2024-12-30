4c0da75dc63a1079a035e9d51ffda8ca530e6d90
lorypelli <lorypelli@proton.me>
2024-12-30 08:38:16 +0100

    `outDir` is `dist` by default

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
     entry: ['scripts/index.ts'],
-    outDir: './dist',
     target: 'node14.13.1',
     format: 'esm',
     minify: true,
