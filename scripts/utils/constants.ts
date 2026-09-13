e25da22b6b1cd04f69b2146fbb2e14c584f7909a
lorypelli <lorypelli@proton.me>
2026-09-13 16:53:30 +0200

    fix different arch and drop readme

--- scripts/utils/constants.ts
+++ scripts/utils/constants.ts
@@ -1,6 +1,24 @@
 import { tmpdir } from 'node:os';
 import { join } from 'node:path';
 import { arch, platform } from 'node:process';
+import { error } from './logs.ts';
+
+const goos: Partial<Record<NodeJS.Platform, string>> = {
+    darwin: 'darwin',
+    linux: 'linux',
+    win32: 'windows',
+};
+
+const goarch: Partial<Record<NodeJS.Architecture, string>> = {
+    arm64: 'arm64',
+    x64: 'amd64',
+};
+
+const os = goos[platform];
+
+const cpu = goarch[arch];
+
+if (!os || !cpu) error(`Unsupported platform: ${platform}/${arch}`);
 
 export const extension = platform == 'win32' ? '.exe' : '';
 
@@ -8,6 +26,4 @@ export const file = join(tmpdir(), `server${extension}`);
 
 export const etag = `${file}.etag`;
 
-const suffix = arch == 'arm64' && platform != 'win32' ? '_arm64' : '';
-
-export const url = `https://github.com/lorypelli/server/releases/latest/download/server_${platform}${suffix}${extension}`;
+export const url = `https://github.com/lorypelli/server/releases/latest/download/server_${os}_${cpu}${extension}`;
